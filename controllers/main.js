e5a722f44e8f8d13ad2d9192a7fa00df6ab3ded4
ThilakRamanie <thilakramanie007@gmail.com>
2024-01-21 21:57:01 +0000

    Completed Project

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-  res.send("Fake Login/Register/SignUp");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "User created", token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).send({
-    msg: `Hello, John Doe`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your lucky number your lucky number is ${luckyNumber}`,
   });
 };
